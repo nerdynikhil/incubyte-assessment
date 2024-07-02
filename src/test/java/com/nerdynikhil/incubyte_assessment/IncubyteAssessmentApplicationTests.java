624f6c0e93c5ddc7b90e479a831bb9eac3741798
Nikhil Barik <nklmarch17@gmail.com>
2024-07-02 12:35:54 +0530

    Initial commit

---
+++ src/test/java/com/nerdynikhil/incubyte_assessment/IncubyteAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdynikhil.incubyte_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
